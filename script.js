0eb118f8edd22f6da99a613044c87a6bf214ba86
Slake37 <moldovan.alin160@yahoo.co.uk>
2021-09-01 21:47:13 +0100

    new

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 let hamMenu = document.querySelector('.ham-menu');
 let links = document.querySelectorAll('.link-btn');
 let closeBtn = document.querySelector('.close-btn');
-let navLinks = document.querySelector('.navigation-links')
+let navLinks = document.querySelector('.navigation-links');
+let inputName = document.querySelector('.input-name');
+let inputAreaName = document.querySelector('.input-area-name');
+let inputEmail = document.querySelector('.input-email');
+let inputAreaEmail = document.querySelector('.input-area-email');
+let inputMessage = document.querySelector('.input-message');
+let inputAreaMessage = document.querySelector('.input-area-message');
 
 hamMenu.addEventListener('click', function(){
     navLinks.classList.remove('close');
@@ -16,4 +22,18 @@ closeBtn.addEventListener('click', function(){
 navLinks.addEventListener('click', function(){
     navLinks.classList.add('close');
     navLinks.classList.remove('open');
+});
+
+
+inputAreaName.addEventListener('click', function(){
+    inputName.classList.add('select-label')
 })
+
+inputAreaEmail.addEventListener('click', function(){
+    inputEmail.classList.add('select-label')
+})
+
+inputAreaMessage.addEventListener('click', function(){
+    inputMessage.classList.add('select-label')
+})
+
